68671b16a8b79e6b8b5bab7926e35fdbec5bce2b
Blairoo <odhlmj@naver.com>
2024-01-31 14:23:19 +0900

    if문

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
+	}
+	return true
+}
 
 func main() {
-	fmt.Println("hello main")
+	fmt.Println(canIDrink(16))
 }
