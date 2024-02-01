d2a5583e2a681ef42b7fdd464347f08e14ca8ffc
Blairoo <odhlmj@naver.com>
2024-02-01 18:29:40 +0900

    map 공부중

--- main.go
+++ main.go
@@ -4,13 +4,7 @@ import (
 	"fmt"
 )
 
-func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 {
-		return false
-	}
-	return true
-}
-
 func main() {
-	fmt.Println(canIDrink(16))
+	nico := map[string]string{"name": "nico", "age": "12"}
+	fmt.Println(nico)
 }
