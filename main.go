48d785f83e03c15478aaca728507ce149cae3504
Blairoo <87243056+Blairoo@users.noreply.github.com>
2024-02-10 04:49:00 +0900

    map 을 dictionary 로 활용

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 
-	"github.com/blairoo/learngo/accounts"
+	"github.com/blairoo/learngo/mydict"
 )
 
 func main() {
-	account := accounts.NewAccount("nico")
-	account.Deposit(10)
-
-	fmt.Println(account)
+	dictionary := mydict.Dictionary{}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(word)
 }
